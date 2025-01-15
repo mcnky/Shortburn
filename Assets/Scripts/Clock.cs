519aac5f63ddd730f7d7af4b46af4a11b61b7372
mcnky <vincent_krook@hotmail.com>
2025-01-15 11:04:56 +0100

    player size fixed -- clock finished

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    public List<Transform> clockPoints;
+    public List<Transform> clockPoints; 
     public Transform hourHand;
     public Transform minuteHand;
     public float timeSpeed = 1f;
@@ -17,7 +17,7 @@ public class Clock : MonoBehaviour
     {
         elapsedTime += Time.deltaTime * timeSpeed;
 
-        if (elapsedTime >= 1f)
+        if (elapsedTime >= 1f) 
         {
             elapsedTime = 0f;
             currentMinute++;
@@ -41,19 +41,27 @@ public class Clock : MonoBehaviour
     {
         if (clockPoints.Count < 12) return;
 
-        int minuteIndex = currentMinute / 5; 
-        int hourIndex = currentHour % 12; 
+        
+        int minuteIndex = Mathf.RoundToInt(currentMinute / 5f) % 12;
 
-        if (minuteHand && clockPoints[minuteIndex])
+        
+        int hourIndex = currentHour % 12;
+
+        
+        if (minuteHand)
         {
-            Vector3 direction = clockPoints[minuteIndex].position - minuteHand.position;
-            minuteHand.up = direction;
+            Transform targetMinutePoint = clockPoints[minuteIndex];
+            Vector3 direction = targetMinutePoint.position - minuteHand.position;
+            float angle = Mathf.Atan2(direction.y, direction.z) * Mathf.Rad2Deg; 
+            minuteHand.rotation = Quaternion.Euler(-angle, 0, 0); 
         }
 
-        if (hourHand && clockPoints[hourIndex])
+        if (hourHand)
         {
-            Vector3 direction = clockPoints[hourIndex].position - hourHand.position;
-            hourHand.up = direction;
+            Transform targetHourPoint = clockPoints[hourIndex];
+            Vector3 direction = targetHourPoint.position - hourHand.position;
+            float angle = Mathf.Atan2(direction.y, direction.z) * Mathf.Rad2Deg; 
+            hourHand.rotation = Quaternion.Euler(-angle, 0, 0); 
         }
     }
 }
